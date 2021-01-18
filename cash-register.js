94aa713a82565eb0b769251fbb482723be8131a2
Ay-slim <ayooluwaadedipe@gmail.com>
2021-01-18 08:25:39 +0100

    Fixed bugs, solutions work now

--- cash-register.js
+++ cash-register.js
@@ -1,36 +1,39 @@
-function checkCashRegister(price, cash, cid) {
-  const dueChange = cash - price;
-  const changeObj = {}
-  for (let j = cid.length-1;j>=0;j--){
-    changeObj[cid[j][0]] = cid[j][1]
-  }
-  let returnObject = {status: null, change:[]}
-  const drawerSum = cid.reduce((acc, current)=>{
-    return acc+current[1]
+function checkCashRegister (price, cash, cid) {
+  let dueChange = (cash - price) * 100
+  const returnObject = {}
+  const changeArray = []
+  const denominations = [1, 5, 10, 25, 100, 500, 1000, 2000, 10000]
+  denominations.reverse()
+  const reversedCid = cid.reverse().map(function (currentValue) { return [currentValue[0], currentValue[1] * 100] })
+  const drawerSum = reversedCid.reduce((acc, current) => {
+    return acc + (current[1])
   }, 0)
-  if (drawerSum < dueChange){
-    returnObject['status'] = 'INSUFFICIENT_FUNDS'
+  if (drawerSum === dueChange) {
+    returnObject.status = 'CLOSED'
+    returnObject.change = cid.reverse()
     return returnObject
   }
-  if (drawerSum === dueChange){
-    returnObject['status'] = 'CLOSED'
-    returnObject['change'] = cid
-  }
-  returnObject['status'] = 'OPEN'
-  while (dueChange > 0){
-
-    for (let i in changeObj){
-      if (changeObj[i]===0 || dueChange < changeObj[i]){
-        returnObject['change'].push([i, changeObj[i]])
-      }
-      else if (dueChange >= changeObj[i]){
-        returnObject['change'].push([i, changeObj[i]])
-        dueChange -= changeObj[i]
-      }
+  console.log(reversedCid)
+  for (let j = 0; j < reversedCid.length; j++) {
+    let denominationCounter = 0
+    while (dueChange >= denominations[j] && reversedCid[j][1] > 0) {
+      reversedCid[j][1] -= denominations[j]
+      dueChange -= denominations[j]
+      denominationCounter += denominations[j]
+    }
+    if (denominationCounter) {
+      changeArray.push([cid[j][0], denominationCounter / 100])
     }
   }
+  const totalChange = changeArray.reduce((acc, currValue) => (currValue[1] + acc), 0)
+  if (totalChange < (cash - price)) {
+    returnObject.status = 'INSUFFICIENT_FUNDS'
+    returnObject.change = []
+    return returnObject
+  }
+  returnObject.status = 'OPEN'
+  returnObject.change = changeArray
   return returnObject
-
 }
 
-console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]))
+console.log(checkCashRegister(19.5, 20, [['PENNY', 0.5], ['NICKEL', 0], ['DIME', 0], ['QUARTER', 0], ['ONE', 0], ['FIVE', 0], ['TEN', 0], ['TWENTY', 0], ['ONE HUNDRED', 0]]))
